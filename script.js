ba4c89fe496e1b7bb7ecfb5c62d990cc748917b2
pino <pipi74miyak@gmail.com>
2026-09-17 00:27:15 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ addButton.addEventListener("click", function () {
 
     const questName = prompt("新しいQuestの名前を入力してください");
 
-const newTodo = document.createElement("p");
+    const newTodo = document.createElement("p");
+
+    newTodo.textContent = "□" + questName;
+
+    todoList.appendChild(newTodo);
 
 });
