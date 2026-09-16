a1d61a54d2d75496cfa875b3fad7d7b6fce2aa85
pino <pipi74miyak@gmail.com>
2026-09-16 23:13:06 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-// Quest追加ボタンを取得
-const addButton = document.getElementById("add-quest");
+const addButton = document.getElementById("add-button");
 
-// Quest追加ボタンが押されたとき
 addButton.addEventListener("click", function () {
-    const questName = prompt("新しいQuestの名前を入力してください");
 
-    // 何も入力されなかった場合
-    if (questName === null || questName.trim() === "") {
-        return;
-    }
-
-    alert("Quest「" + questName + "」を追加します！");
 });
