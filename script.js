4772c5393de1ab9f545e7efee3e0c2145c716d4a
pino <pipi74miyak@gmail.com>
2026-09-17 00:16:00 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ addButton.addEventListener("click", function () {
 
     const questName = prompt("新しいQuestの名前を入力してください");
 
-    alert("入力されたQuest：" + questName);
+    alert("入力されたQues：" + questName);
 
 });
